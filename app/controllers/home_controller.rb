52b02d9a0ee62b5d5ccb060092ed802b0c617225
Shirakura-Kazuki <ok210192@oks.iput.ac.jp>
2023-10-14 22:18:17 +0900

    checkj_point

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -19,28 +19,42 @@ class HomeController < ApplicationController
 
     def open_pdf
         # パラメータをローカル変数に格納
-        document_id = params[:id]
-        filename = params[:pdf_filename]
-        author = params[:author]
-        file_url = params[:pdf_file_id]
+        @keycode = params[:keycode]
+        @file_id = params[:id]
 
         # パラメータの存在確認
-        unless file_url
+        unless @file_id
             render json: { error: "受信待ち" }, status: :bad_request
             return
         end
     
         # PDFのURLを修正
-        original_url = file_url.to_s
-        modified_url =  "https://drive.google.com/file/d/"+ original_url + "/preview"
+        original_url = @file_id.to_s
+        # modified_url =  "https://drive.google.com/file/d/"+ original_url + "/preview"
+        @modified_url =  "https://drive.google.com/file/d/"+ original_url + "/preview"
+        
         
         # Pusherでイベントをトリガー
         begin
-            Pusher.trigger('my-channel', 'my-event', { message: modified_url })
+            Pusher.trigger('my-channel', 'my-event', { message: @modified_url })
             head :ok
         rescue => e
             render json: { error: "Pusherでエラーが発生しました: #{e.message}" }, status: :internal_server_error
         end
+
+        # 外部スクリプトへ値を受け渡す
+        ENV['KEYCODE'] = @keycode
+        ENV['FILE_ID'] = @file_id
+
+        # #PDFファイルの保存先指定（public限定）
+        # ENV['DOWNLOAD_PATH'] = Rails.root.join('public', 'download.pdf').to_s
+
+        # 外部のRubyスクリプトを実行
+        system("ruby C:/Users/s_k_t/pusher_test1010_app/config/download.rb")
+
+        
+        
+
     end
   
   
--- config/download.rb
+++ config/download.rb
@@ -3,6 +3,7 @@ require 'uri'
 require 'json'
 require "base64"
 
+
 # AHT21B.pdf					1AQpqlhCGrqAwLfwGrgqxQa3zUrkKJJjZ
 # AHT25.pdf					    1KdtZuBJhlxBOh25uqcrinL1nLe9DR6mm
 # AE-BME280_manu_v1.1.pdf		14k1xYaEQ12HamdnxflP5lmaI0EySonsw
@@ -10,7 +11,11 @@ require "base64"
 # AE-AMG8833-BO_20210820.pdf    1TR97KJh0AJf1D5xC0ciik2Vy7rbauoi3
 
 #自分用                         1L1WsAs7RwQ0NlTfeR6QcKJb47T8YBZAG  
-#                               1dmD5LNIOFQd0gjkLhRXKXEFF1bzraCCF
+#ダウンロードファイル            1Cb3pJG-8A6LCY8XvlXl_vgCjPk9OoJxu
+#                              1t8OueyGZqRhKoWVKWNUmrvqJ-EUmQPog
+
+keycode_s = ENV['KEYCODE']
+modified_url_s = ENV['FILE_ID'] 
 
 response = Net::HTTP.post(
     #URI('https://script.google.com/macros/s/AKfycbx03ht4TI8bcMcBFhuR46EMvwImDLrRBgtNQLUGUBdvbUV6SERc7eSWZCz5cZzm9kXbZg/exec'),
@@ -18,14 +23,18 @@ response = Net::HTTP.post(
     URI('https://script.google.com/macros/s/AKfycbzo7SHpPmcN0wcaicmINs2o5nesndA0hoNKSLSwPao98MrUFqY-mcxnHm3u5M1IF526/exec'),
     
     #GASAPIのキーコードとダウンロードするファイルのIDを定義
-    {keycode:'download_SO1AZAhF9C1o7kaB52CK', id:'1dmD5LNIOFQd0gjkLhRXKXEFF1bzraCCF'}.to_json,
+    #{keycode:'download_SO1AZAhF9C1o7kaB52CK', id:'1Cb3pJG-8A6LCY8XvlXl_vgCjPk9OoJxu'}.to_json,
+     {keycode:keycode_s , id:modified_url_s}.to_json,    
     'Content-Type' => 'application/json'
 )
+puts modified_url_s + ":add"
+puts keycode_s +":add"
 case response
 when Net::HTTPSuccess
     # 何もしない
 when Net::HTTPRedirection
     puts 'redirect!'
+
     # リダイレクト対応
     response = Net::HTTP.get_response(URI.parse(response['location']))
     case response
@@ -39,16 +48,28 @@ else
 end
 
 if response.nil?
-    puts 'ERROR!'
+    puts 'ERROR!Response is nil'
     return
 end
 
+puts "Response Body: #{response.body}"
 # {message:'succeeded', base64:'...'}
 params = JSON.parse(response.body);
+
+if params['base64'].nil?
+    puts 'ERROR: base64 is nil'
+    return
+end
 
 puts params['message']
 
 # ファイルに保存
+
+# download_path = ENV['DOWNLOAD_PATH']
+# File.open(download_path, "wb") do |file|
+#     file.write(Base64.decode64(params['base64']))
+# end
+
 File.open("download.pdf", "wb") do |file|
     file.write(Base64.decode64(params['base64']))
 end
