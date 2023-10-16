e4a2b52a65c4c0ce068ff508315d313147e4e23d
Shirakura-Kazuki <ok210192@oks.iput.ac.jp>
2023-10-16 23:20:41 +0900

    day_check

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -22,32 +22,30 @@ class HomeController < ApplicationController
         @keycode = params[:keycode]
         @file_id = params[:id]
 
-        # パラメータの存在確認
-        if @file_id
-            render json: { message: "処理を開始します" }, status: :ok
-        else
-            render json: { error: "受信待ち" }, status: :bad_request
-            return
-        end
+        # # パラメータの存在確認
+        # if @file_id
+        #     render json: { message: "処理を開始します" }, status: :ok
+        # else
+        #     render json: { error: "受信待ち" }, status: :bad_request
+        #     return
+        # end
     
         # PDFのURLを修正
         original_url = @file_id.to_s
         @modified_url =  "https://drive.google.com/file/d/"+ original_url + "/preview"
 
         # #Pusherでイベントをトリガー
-        # begin
-        #     Pusher.trigger('my-channel', 'my-event', { message: 'new_pdf_generated' })
-        #     head :ok
-        # rescue => e
-        #     render json: { error: "Pusherでエラーが発生しました: #{e.message}" }, status: :internal_server_error
-        # end
+        begin
+            Pusher.trigger('my-channel', 'my-event', { message:"処理を開始します" })
+            head :ok
+        rescue => e
+            render json: { error: "Pusherでエラーが発生しました: #{e.message}" }, status: :internal_server_error
+        end
 
 
         # 外部スクリプトへ値を受け渡す
         ENV['KEYCODE'] = @keycode
         ENV['FILE_ID'] = @file_id
-        @i = 1
-        ENV['index'] = @i.to_s
 
         # PDFファイルの保存先指定（public/temp/ディレクトリ内）
         ENV['DOWNLOAD_PATH'] = Rails.root.join('public', 'temp', 'download.pdf').to_s
--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -1,35 +1,37 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var pusher = new Pusher('32f2640be6b94e4b6afe', {
-      cluster: 'ap3'
-    });
-  
-    var channel = pusher.subscribe('my-channel');
-    
-    //my-event:トリガー関数
-    channel.bind('my-event', function(data) {
-      alert('Received my-event with message: ' + data.message);
-    });
-  
-    // document.getElementById("send-notification").addEventListener("click", function() {
-    //   fetch('/send_notification', {
-    //     method: 'POST',
-    //     headers: {
-    //       'Content-Type': 'application/json',
-    //       'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').content
-    //     }
-    //   });
-    // });
-
-    // 動作test用：プッシュ通知定義
-
-    document.getElementById("send-notification").addEventListener("click", function() {
-        fetch('/send_pdf', {    //routes.rb定義
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').content
-          }
-        });
-      });
+  var pusher = new Pusher('32f2640be6b94e4b6afe', {
+    cluster: 'ap3'
   });
+
+  var channel = pusher.subscribe('my-channel');
+
+  channel.bind('my-event', function(data) {
+    alert('Received my-event with message: ' + data.message);
+  });
+
+  // my-event: Custom Trigger Function
+  channel.bind('my-event', function(data) {
+    // Create and display the notification
+    var notification = document.createElement('div');
+    notification.innerText = 'Received my-event with message: ' + data.message;
+    notification.style.position = 'fixed';
+    notification.style.top = '50%';
+    notification.style.left = '50%';
+    notification.style.transform = 'translate(-50%, -50%)';
+    notification.style.backgroundColor = '#ffffff';
+    notification.style.border = '1px solid #ccc';
+    notification.style.padding = '15px';
+    notification.style.width = '200px';
+    notification.style.height = '100px';  // Changed from 300px to 100px
+    notification.style.zIndex = '1000';
+    document.body.appendChild(notification);
+
+    // Remove the notification after 3 seconds
+    setTimeout(function() {
+      document.body.removeChild(notification);
+    }, 3000);
+  });
+});
+
+
   
